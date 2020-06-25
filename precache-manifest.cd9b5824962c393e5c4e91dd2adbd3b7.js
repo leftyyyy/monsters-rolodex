9818ebe3d0a1aabccc8df3720e93a78e2a0c7ce6
Rishi Prajapati <leftyyyy@users.noreply.github.com>
2020-06-25 16:58:50 +0530

    Updates

---
+++ precache-manifest.cd9b5824962c393e5c4e91dd2adbd3b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74991bd6e1b7f5c1a6c18a9459831237",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "48abcfd96f9d7c6d01bf",
+    "url": "/monsters-rolodex/static/css/main.c23cd604.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48abcfd96f9d7c6d01bf",
+    "url": "/monsters-rolodex/static/js/main.c261ec84.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
